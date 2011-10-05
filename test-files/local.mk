fb9ad7f793c828ef821a1c12c26c7df934ecac57
Artur Adib <arturadib@gmail.com>
2011-10-05 17:39:22 -0400

    bot_test bot_master

--- test-files/local.mk
+++ test-files/local.mk
@@ -1,9 +1,16 @@
 # make master
 #
 # Generates a snapshot reference set
-master:
+bot_master:
 	cd test && \
 	python -u test.py \
 	--masterMode \
 	--noPrompts \
 	--browserManifestFile=$(PDF_BROWSERS)
+
+# Generates a snapshot reference set
+bot_test:
+	cd test; \
+	python -u test.py \
+	--browserManifestFile=$(PDF_BROWSERS) \
+	--manifestFile=$(PDF_TESTS)
