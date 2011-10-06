a2418638a100c7ae1b6fc0ffb3aa8bdca280be3a
Artur Adib <arturadib@gmail.com>
2011-10-06 08:43:42 -0400

    Cleaning up; removed scripts.js

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ var fs = require('fs'),
     express = require('express'),
     app = express.createServer(),
     path = require('path'),
-    request = require('request'),    
+    request = require('request'),
+    exec = require('child_process').exec,
     github = require('./lib/github'),
-    scripts = require('./lib/scripts'),
     queue = require('./lib/queue');
 
 var config = {},
@@ -57,11 +57,13 @@ function setupServer(callback){
   // HTTP server
   app.use(express.bodyParser());
   app.use(app.router);
-  app.use(express.static(__dirname+'/'+config.dest_path));
+  app.use(express.static(__dirname+'/'+config.tmp_path));
   app.listen(config.server_port);
-  console.log((new Date())+': HTTP server listening on port '+config.server_port+', serving dir '+config.dest_path);
 
-  getPublicIP(function(){        
+  getPublicIP(function(){
+    config.server_url = 'http://'+config.server_host+':'+config.server_port;
+    console.log((new Date())+': Server listening at '+config.server_url+', serving dir '+config.tmp_path);
+
     // Github-specific
     github.setConfig(config);
     github.buildWhitelist(function(){
@@ -78,8 +80,6 @@ function setupServer(callback){
 function getPublicIP(callback){
   request.get('http://ifconfig.me/ip', function(error, response, body){
     config.server_host = body.replace(/\s|\n/g, '');
-    console.log((new Date())+': using public IP '+config.server_host);
-
     if (callback) callback();
   });
 };
@@ -91,11 +91,12 @@ function getPublicIP(callback){
 function processNewCommands(){  
   // Loop over every new command in all monitored pull requests
   github.forEachNewCommand(function(cmd){
-    var t1 = new Date();
+    var t1 = new Date(),
+        liveOutputFile = cmd.id+'.txt';
     
     console.log((new Date())+': processing new command "'+cmd.command+'" in Pull #'+cmd.pull_number+' from @'+cmd.user+' (id:'+cmd.id+'), queue size: '+queue.size());    
     github.postStartMessage(cmd, 'Processing command **'+(cmd.command||'(empty)')+'** by user _'+cmd.user+'_. Queue size: '+queue.size()+'\n\n'+
-                                 'Live script output is available at: '+'http://'+config.server_host+':'+config.server_port+'/'+cmd.pull_sha+'.txt');
+                                 'Live script output is available at: '+config.server_url+'/'+liveOutputFile);
     
     //
     // Process each command in queue
@@ -109,14 +110,16 @@ function processNewCommands(){
         // Process 'test' command
         //      
         case 'test':
-          scripts.runTest(
+          runScript(
             {
+              script: cmd.command,
               main_url: 'git://github.com/'+config.main_repo+'.git',
               pull_url: cmd.pull_url,
               pull_sha: cmd.pull_sha,
               ref_url: 'git://github.com/'+config.ref_repo+'.git',
-              dest_path: config.dest_path,
-              timeout: config.process_timeout_mins*60*1000
+              tmp_path: config.tmp_path,
+              timeout: config.process_timeout_mins*60*1000,
+              output_file: liveOutputFile
             }, 
             function(output){
               var msg = '';
@@ -140,12 +143,10 @@ function processNewCommands(){
 
                 msg = '**ERROR(s) found**';
 
-                if (path.existsSync(config.dest_path+'/tests/'+cmd.pull_sha+'/eq.log')) {
-                  
-                  var url = 'http://'+config.server_host+':'+config.server_port+'/tests/'+cmd.pull_sha+'/reftest-analyzer.xhtml'+
+                if (path.existsSync(config.tmp_path+'/tests/'+cmd.pull_sha+'/eq.log')) {                  
+                  var url = config.server_url+'/tests/'+cmd.pull_sha+'/reftest-analyzer.xhtml'+
                             '#web=/tests/'+cmd.pull_sha+'/eq.log';
                   msg += '\n\n**ATTENTION:** There was a _snapshot difference:_\n'+url;
-
                 }
               } // if !tests passed
 
@@ -162,14 +163,16 @@ function processNewCommands(){
         // Process 'makeref' command
         //      
         case 'makeref':
-          scripts.runMakeref(
+          runScript(
             {
+              script: cmd.command,
               main_url: 'git://github.com/'+config.main_repo+'.git',
               pull_url: cmd.pull_url,
               pull_sha: cmd.pull_sha,
               ref_url: 'git://github.com/'+config.ref_repo+'.git',
-              dest_path: config.dest_path,
-              timeout: config.process_timeout_mins*60*1000
+              tmp_path: config.tmp_path,
+              timeout: config.process_timeout_mins*60*1000,
+              output_file: liveOutputFile              
             }, 
             function(output){
               var msg = '';
@@ -217,3 +220,33 @@ function processNewCommands(){
   }); // forEachNewCommand
 }; // checkNewMessages()
 
+//
+// runScript()
+//
+function runScript(args, callback){
+  var outputFile = args.tmp_path+'/'+args.output_file,
+      script = './run-'+args.script,
+      cmd = 'mkdir -p '+args.tmp_path+'; '+
+            script+' '+args.main_url+' '+args.pull_url+' '+args.pull_sha+' '+args.ref_url+' '+args.tmp_path+
+            ' 2>&1 | tee '+outputFile;
+
+  console.log((new Date())+': running: '+cmd); 
+
+  //
+  // Launch process
+  //
+  exec(cmd, 
+    {
+      timeout: args.timeout || undefined
+    },
+    function(error, stdout, stderr){
+      //
+      // Script done
+      //
+      if (error && error.killed) {
+        stdout += '\n\n***\nProcess killed (timeout).\n';
+      }
+      if (callback) callback(stdout+'\n\n_____________________________ stderr:\n\n'+stderr);
+    }
+  );
+}; // runScript()
